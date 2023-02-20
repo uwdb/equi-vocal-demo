3f5cb5bf005084903979cf6d318481958eea676d
Enhao Zhang <enhaoz@cs.washington.edu>
2023-02-20 15:26:55 -0800

    replace HTML DOM with jQuery

--- equi_app/static/equi_app/js/main.js
+++ equi_app/static/equi_app/js/main.js
@@ -1,30 +1,32 @@
 // TODO: buttons in the main container should be fixed to the bottom of the page (not as part of the scrollable area)
 
+const createSampleInput = (segment_src, segment_label, i) => {
+    return $(`
+        <div class="card m-1">
+            <video width="240" controls autoplay loop muted class="p-2">
+                <source src="${segment_src}" type="video/mp4"> Your browser does not support the video tag.
+            </video>
+            <div class="card-body btn-group btn-group-sm" role="group" aria-label="Binary label of the video segment">
+                <input type="radio" class="btn-check" name="btnradio_${i}" id="btnradio1_${i}" autocomplete="off" ${segment_label == 1 ? 'checked' : 'disabled'}>
+                <label class="btn btn-outline-primary" for="btnradio1_${i}">Positive</label>
+
+                <input type="radio" class="btn-check" name="btnradio_${i}" id="btnradio2_${i}" autocomplete="off" ${segment_label == 1 ? 'disabled' : 'checked'}>
+                <label class="btn btn-outline-primary" for="btnradio2_${i}">Negative</label>
+            </div>
+        </div>
+    `);
+}
+
 function showMoreSegments() {
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", "show_more_segments/", true);
-    xhttp.onload = function() {
+    xhttp.onload = function () {
         var data = JSON.parse(this.response);
         var segments = data.video_paths;
         var selected_gt_labels = data.labels;
-        // var gallery = document.getElementById("gallery");
-        // gallery.innerHTML = "";
         $("#gallery").empty();
         for (var i = 0; i < segments.length; i++) {
-            $("#gallery").append(`
-                <div class="card m-1">
-                    <video width="240" controls autoplay loop muted class="p-2">
-                        <source src="${segments[i]}" type="video/mp4"> Your browser does not support the video tag.
-                    </video>
-                    <div class="card-body btn-group btn-group-sm" role="group" aria-label="Binary label of the video segment">
-                        <input type="radio" class="btn-check" name="btnradio_${i}" id="btnradio1_${i}" autocomplete="off" ${selected_gt_labels[i] == 1 ? 'checked': 'disabled'}>
-                        <label class="btn btn-outline-primary" for="btnradio1_${i}">Positive</label>
-
-                        <input type="radio" class="btn-check" name="btnradio_${i}" id="btnradio2_${i}" autocomplete="off" ${selected_gt_labels[i] == 1? 'disabled' : 'checked'}>
-                        <label class="btn btn-outline-primary" for="btnradio2_${i}">Negative</label>
-                    </div>
-                </div>
-            `);
+            $("#gallery").append(createSampleInput(segments[i], selected_gt_labels[i], i));
         }
     };
     xhttp.send();
@@ -39,17 +41,15 @@ function iterativeSynthesis(init) {
     }
     var xhttp = new XMLHttpRequest();
     xhttp.open("GET", url, true);
-    xhttp.onload = function() {
+    xhttp.onload = function () {
         var data = JSON.parse(this.response);
+        var main_container = $("#main-container");
         if (data.terminated == true) {
             // Update the gallery
-            var main_container = document.getElementById("main-container");
-            var heading = document.createElement("h5");
-            heading.classList.add("pt-5", "pb-5");
-            heading.innerHTML = `Algorithm terminated.`;
-            main_container.appendChild(heading);
+            var heading = $("h5").addClass("pt-5", "pb-5").text("Algorithm terminated.");
+            main_container.append(heading);
             // Update button
-            document.getElementById("btn").remove();
+            $("#btn").remove();
             // TODO: Add a button to restart.
         }
         else {
@@ -62,64 +62,43 @@ function iterativeSynthesis(init) {
             var best_query = data.best_query;
             var best_score = data.best_score;
             // Append to the main container
-            var main_container = document.getElementById("main-container");
             // Update heading
-            var heading = document.createElement("h5");
-            heading.innerHTML = `Iteration ${iteration}`;
-            heading.classList.add("pt-5");
-            main_container.appendChild(heading);
+            var heading = $("<h5></h5>").addClass("pt-5").text(`Iteration ${iteration}`);
+            main_container.append(heading);
             // Update stats
-            var stats = document.createElement("div");
-            stats.classList.add("alert", "alert-info", "mt-3");
-            stats.innerHTML = `
+            var stats = $("<div></div>").addClass("alert alert-info mt-3").html(`
                 <strong>Current iteration</strong>: ${iteration}
                 <br>
                 <strong>Current positive examples</strong>: ${current_npos}
                 <br>
                 <strong>Current negative examples</strong>: ${current_nneg}
-            `;
-            main_container.appendChild(stats);
+            `);
+            main_container.append(stats);
             // Update the top-k query
-            var top_k_queries_div = document.createElement("div");
-            top_k_queries_div.classList.add("alert", "alert-info");
-            top_k_queries_div.innerHTML = `
+            var top_k_queries_div = $("<div></div>").addClass("alert alert-info").html(`
                 <strong>Top-1 query</strong>: ${best_query}
                 <br>
                 <strong>Top-1 score</strong>: ${best_score}
-            `;
-            main_container.appendChild(top_k_queries_div);
+            `);
+            main_container.append(top_k_queries_div);
             // Update gallery
-            var gallery = document.createElement("div");
-            gallery.classList.add("d-flex", "flex-wrap", "border", "border-1", "border-secondary");
+            var gallery = $("<div></div>").addClass("d-flex flex-wrap border border-1 border-secondary");
             for (var i = 0; i < segments.length; i++) {
-                var video = document.createElement("video");
-                video.setAttribute("width", "240");
-                video.setAttribute("controls", "");
-                video.setAttribute("autoplay", "");
-                video.setAttribute("loop", "");
-                video.setAttribute("muted", "")
-                video.classList.add("p-2");
-                var source = document.createElement("source");
-                source.setAttribute("src", segments[i]);
-                source.setAttribute("type", "video/mp4");
-                source.innerHTML = " Your browser does not support the video tag.";
-                video.appendChild(source);
-                gallery.appendChild(video);
+                gallery.append(createSampleInput(segments[i], selected_gt_labels[i], i));
             }
-            main_container.appendChild(gallery);
+            main_container.append(gallery);
             // Update button
-            document.getElementById("btn").remove();
-            var button_div = document.createElement("div");
-            button_div.id = "btn";
-            button_div.classList.add("d-flex", "justify-content-center", "pt-5", "pb-5");
-            var button = document.createElement("button");
-            button.setAttribute("type", "button");
-            button.classList.add("btn");
-            button.classList.add("btn-outline-primary");
-            button.setAttribute("onclick", "iterativeSynthesis()");
-            button.innerHTML = "Confirm labels";
-            button_div.appendChild(button);
-            main_container.appendChild(button_div);
+            $("#btn").remove();
+            var button_div = $("<div></div>")
+                .addClass("d-flex justify-content-center pt-5 pb-5")
+                .attr('id', 'btn');
+            var button = $("<button></button>")
+                .addClass("btn btn-outline-primary")
+                .attr('onclick', "iterativeSynthesis()")
+                .attr('type', 'button')
+                .text("Confirm labels");
+            button_div.append(button);
+            main_container.append(button_div);
             // Update prediction
             // var prediction = document.getElementById("prediction");
             // prediction.innerHTML = "Predicted positive segments:<br>";
