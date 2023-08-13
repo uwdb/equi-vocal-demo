26c491ef064e613cfdce43c3a541698b39ab18fb
Enhao Zhang <enhaoz@cs.washington.edu>
2023-08-13 16:00:38 -0700

    np

--- equi_site/settings.py
+++ equi_site/settings.py
@@ -90,7 +90,7 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
         'NAME': 'myinner_db',
-        'USER': 'enhaoz',
+        'USER': 'zhangenhao',
         'HOST': '127.0.0.1',
         'PORT': '5432',
     }
